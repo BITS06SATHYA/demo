7c5e6a6cfd27ae27c6e1e12a24a9327e4685cfcf
neelasathya <neelasathya06@gmail.com>
2022-01-27 15:43:27 +0530

    HI message from message runner

--- src/main/java/com/example/demo/cmdRunner/MessageRunner.java
+++ src/main/java/com/example/demo/cmdRunner/MessageRunner.java
@@ -7,6 +7,7 @@ public class MessageRunner implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		System.out.println("Hi");
 		System.out.println("Hi message Runner from cmd line");
 	}
 	
