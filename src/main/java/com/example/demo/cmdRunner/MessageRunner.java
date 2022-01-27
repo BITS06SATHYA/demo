33af7688b1f0c17a3ca5552124f7d2806e67ed45
neelasathya <neelasathya06@gmail.com>
2022-01-27 15:17:00 +0530

    My First Commit on github.

---
+++ src/main/java/com/example/demo/cmdRunner/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.example.demo.cmdRunner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hi message Runner from cmd line");
+	}
+	
+	
+
+}
